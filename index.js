819dae48a96025857f54b2e2fcbb8b8258b033f4
Heru Oktafian <heruoktafian.dot@gmail.com>
2019-06-20 19:47:16 +0700

    Implementation routers function on express framework

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const app = express();
+const routers = require('./routers');
 
-app.get('/', function(req, res){
-    console.log('This GET method created by express.');
-    res.send({
-        name: 'Heru Oktafian',
-        gender: 'Male',
-        age: 31
-    });
-});
+app.use('/api',routers);
 
 app.listen(process.env.port || 5000, function(){
     console.log('This express is listening port 5000 by console log.');
---
+++ routers.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// Endpoint using 'GET' method
+router.get('/students', function(req, res){
+    res.send('GET method.');
+});
+
+// Endpoint using 'POST' method
+router.post('/student', function(req, res){
+    res.send('POST method.');
+});
+
+// Endpoint using 'PUT' method
+router.put('/student/:id', function(req, res){
+    res.send('PUT method.');
+});
+
+// Endpoint using 'DELETE' method
+router.delete('/student/:id', function(req, res){
+    res.send('DELETE method.');
+});
+
+module.exports = router;
