9e35ebadbde57575b6757be9262acedb81a480e4
Heru Oktafian <heruoktafian.dot@gmail.com>
2019-06-20 19:02:05 +0700

    Adding 'GET' method request.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function(){
+    console.log('This GET method created by express.');
+});
+
 app.listen(process.env.port || 5000, function(){
     console.log('This express is listening port 5000 by console log.');
 });
